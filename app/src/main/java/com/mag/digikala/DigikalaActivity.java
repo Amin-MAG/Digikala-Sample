e05bc899236e08b5c9a42c15644e2315e3230b16
Amin-MAG <ghasvari313@gmail.com>
2019-11-08 17:20:11 +0330

    [+] toolbar(view), navigation(view)

--- app/src/main/java/com/mag/digikala/DigikalaActivity.java
+++ app/src/main/java/com/mag/digikala/DigikalaActivity.java
@@ -1,14 +1,24 @@
 package com.mag.digikala;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class DigikalaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_digikala);
+
     }
+
+
+
+
+
+
+
 }
