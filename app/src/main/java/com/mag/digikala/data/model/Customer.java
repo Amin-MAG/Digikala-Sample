fe3e7b033f0ed7a4a8e802430f9efe097a9a258d
Amin-MAG <ghasvari313@gmail.com>
2020-01-09 00:18:00 +0330

    [+] pojo files for customer and order added

---
+++ app/src/main/java/com/mag/digikala/data/model/Customer.java
@@ -0,0 +1,49 @@
+package com.mag.digikala.data.model;
+
+public class Customer {
+
+    private String id;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private  String role;
+    private String username;
+    private String password;
+    private String avatar_url;
+    // Billing
+    // Shipping
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+}
